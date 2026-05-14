947cbf8070985aadb2b6da7183bf48cecb498d5f
ProgrammingSam <programmingsam@gmail.com>
2026-05-14 06:18:48 +0200

    feat(options): add stub defaults files for Flow and Meridian modules

---
+++ options/modules/defaults/OptionsDefaultsMeridian.lua
@@ -0,0 +1,7 @@
+--[[
+    Horizon Suite - Meridian - SetDB routing keys
+    Stub file for the future Meridian module. Add routing key tables here when
+    Meridian options and settings are implemented.
+]]
+local addon = _G.HorizonSuite
+if not addon then return end
