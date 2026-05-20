11e5d2f9169ae7a421241645296041d5e017f4c5
Swift <Anthony.M.Sherman@hotmail.com>
2026-05-20 10:09:01 -0400

    OptionsSearch.lua, Locale Edit

--- options/OptionsSearch.lua
+++ options/OptionsSearch.lua
@@ -7,7 +7,7 @@
 ]]
 local addon = _G.HorizonSuite
 if not addon then return end
-
+local L = addon.L
 local function TokenizeSearchCorpus(str)
     local t = {}
     if not str or str == "" then return t end
@@ -121,7 +121,7 @@ function OptionsData_BuildSearchIndex()
         if cat.key == "Profiles" or cat.key == "Modules" or cat.key == "GlobalToggles" then
             moduleLabel = addon.BrandModule and addon.BrandModule("axis") or "Axis"
         else
-            moduleLabel = addon.BrandModule and addon.BrandModule(moduleKey) or (L and L["MODULES"])
+            moduleLabel = addon.BrandModule and addon.BrandModule(moduleKey) or L["MODULES"]
         end
         local catNameRaw = type(cat.name) == "function" and cat.name() or cat.name
         local catNameStr = tostring(catNameRaw or "")
